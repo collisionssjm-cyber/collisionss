3ce3bd44a5575760c2ca80f0f2d500ceaa229cd6
Joe Marino <collisionss.jm@gmail.com>
2026-02-23 17:50:47 -0500

    Add admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,9 +2,9 @@ import { createClient } from "@supabase/supabase-js";
 
 export default async function AdminPage() {
   const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!
-  );
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
+);
 
   const { data: audits } = await supabase
     .from("audits")
